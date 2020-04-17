11e187461d5575fe1053b5903e4d827174334f6d
fluffymonster <fluffymonster@users.noreply.github.com>
2020-04-17 12:53:22 +1000

    Change back to original

--- rclone/README.md
+++ rclone/README.md
@@ -4,16 +4,12 @@ Rclone bundled as an Home Assistant add-on.
 
 ## About
 
-Fork of (`https://github.com/alex3305/hassio-addons`)
-
-Supporting ARM devices
-
 ## Installation
 
 Follow these steps to get the add-on installed on your system:
 
 1. Navigate in your Home Assistant frontend to __Supervisor -> Add-on Store__
-2. Add this new repository by URL (`https://github.com/flufyfmonster/hassio-addons`)
+2. Add this new repository by URL (`https://github.com/alex3305/hassio-addons`)
 3. Find the "Rclone" add-on and click it.
 4. Click on the "INSTALL" button
 
--- rclone/run.sh
+++ rclone/run.sh
@@ -9,11 +9,11 @@ LOCAL_RETENTION=$(bashio::config 'local_retention_days')
 REMOTE_RETENTION=$(bashio::config 'remote_retention_days')
 
 bashio::log.info "Pruning local files..."
-#find /backup/ -mtime +${LOCAL_RETENTION} -type f -delete
+find /backup/ -mtime +${LOCAL_RETENTION} -type f -delete
 bashio::log.info "Pruning local files finished"
 
 bashio::log.info "Starting remote copy..."
-#rclone -v --config ${RCLONE_CONFIG} \
-#    --max-age "${REMOTE_RETENTION}d" \
-#    --ignore-existing copy /backup/ "${REMOTE}:${REMOTE_PATH}"
+rclone -v --config ${RCLONE_CONFIG} \
+    --max-age "${REMOTE_RETENTION}d" \
+    --ignore-existing copy /backup/ "${REMOTE}:${REMOTE_PATH}"
 bashio::log.info "Remote copy finished"
