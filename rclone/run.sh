1bd291bf5c35522904a02e48115b938d34961e87
fluffymonster <fluffymonster@users.noreply.github.com>
2020-04-17 16:56:35 +1000

    Fix EOL characters

--- rclone/run.sh
+++ rclone/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bashio
+#!/usr/bin/with-contenv bashio
 
 RCLONE_CONFIG=$(bashio::config 'configuration_path')
 
@@ -9,11 +9,12 @@ LOCAL_RETENTION=$(bashio::config 'local_retention_days')
 REMOTE_RETENTION=$(bashio::config 'remote_retention_days')
 
 bashio::log.info "Pruning local files..."
-find /backup/ -mtime +${LOCAL_RETENTION} -type f -delete
+#find /backup/ -mtime +${LOCAL_RETENTION} -type f -delete
 bashio::log.info "Pruning local files finished"
 
 bashio::log.info "Starting remote copy..."
-rclone -v --config ${RCLONE_CONFIG} \
-    --max-age "${REMOTE_RETENTION}d" \
-    --ignore-existing copy /backup/ "${REMOTE}:${REMOTE_PATH}"
+rclone --version
+#rclone -v --config ${RCLONE_CONFIG} \
+#    --max-age "${REMOTE_RETENTION}d" \
+#    --ignore-existing copy /backup/ "${REMOTE}:${REMOTE_PATH}"
 bashio::log.info "Remote copy finished"
